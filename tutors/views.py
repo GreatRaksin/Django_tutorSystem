7f6ec4843f8b8529d4e4b966715e0deb48f4d9b9
ARCHITECT <greatraksin@icloud.com>
2021-08-22 00:59:50 +0300

    Adding filtering

--- tutors/views.py
+++ tutors/views.py
@@ -7,7 +7,11 @@ def index(request):
     form = TutorSelectForm()
     town = request.GET.get('city')
     predm = request.GET.get('subject')
-    tutors = Tutor.objects.filter(city=town, subject=predm)
+
+    if town and predm:
+        tutors = Tutor.objects.filter(city=town, subject=predm)
+    else:
+        tutors = Tutor.objects.all()
 
     return render(request, 'index.html', {'form': form,
                                           'tutors': tutors})
