b30a11f8db6f085e91363cd11e9f6fa8a9eeb39e
ARCHITECT <greatraksin@icloud.com>
2021-08-21 20:40:36 +0300

    Securing

--- tech_100ballov/settings.py
+++ tech_100ballov/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#)dscp4_v!bz-_c!qp$&z7@k-lf1%s^&fbm+y9%6evmmb4f(q3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
